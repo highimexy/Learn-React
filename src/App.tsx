0b1844cbf530768fdab9d239d2fe3b2ed0b3a5ee
highimexy <teslawiktor@gmail.com>
2025-11-01 09:25:25 +0100

    21:11

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./header";
-import Footer from "./footer";
-import Food from "./Food";
+import Header from "./Header.tsx";
+import Footer from "./Footer.tsx";
+import Food from "./Food.tsx";
 
 function App() {
   return (
--- src/Food.tsx
+++ src/Food.tsx
@@ -1,12 +1,12 @@
 function Food() {
-  const food1: string = "Orange";
-  const food2: string = "Banana";
+  const food2: string = "Orange";
+  const food3: string = "Banana";
 
   return (
     <ul>
       <li>Apple</li>
-      <li>{food1}</li>
-      <li>{food2.toUpperCase()}</li>
+      <li>{food2}</li>
+      <li>{food3.toUpperCase()}</li>
     </ul>
   );
 }
