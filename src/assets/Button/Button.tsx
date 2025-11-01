a483315d28d1fc0261b45c4b5706d13f93e186f3
highimexy <teslawiktor@gmail.com>
2025-11-01 10:06:41 +0100

    fix

--- src/assets/Button/Button.tsx
+++ src/assets/Button/Button.tsx
@@ -1,7 +1,7 @@
 import styles from './Button.module.css'
 
 function Button() {
-  return <button className={styles.button}>Click me</button>;
+  return <button className={styles.brobutton}>Click me</button>;
 }
 
 export default Button;
