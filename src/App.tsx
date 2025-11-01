f337da877cfaf664f26d9f36b1f184ba5a6ea17a
highimexy <teslawiktor@gmail.com>
2025-11-01 19:38:05 +0100

    1h:17

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import Button from "./assets/Button/Button.tsx";
-import Student from "./assets/Components/Student.tsx";
-import UserGreeting from "./assets/Components/UserGreeting.tsx";
+import Button from "./Components/Button/Button.tsx";
+import Student from "./Components/Student.tsx";
+import UserGreeting from "./Components/UserGreeting.tsx";
+import List from "./Components/List.tsx";
+
+import type { Fruit } from "./Components/List.tsx";
 
 function App() {
+
+  const fruits: Fruit[] = [
+    { id: 1, name: "Apple", kcal: 50 },
+    { id: 2, name: "Orange", kcal: 60 },
+    { id: 3, name: "Bannana", kcal: 130 },
+    { id: 4, name: "Pineapple", kcal: 140 },
+  ];
   return (
     <>
       <Student name="Sponge Bob" age={30} isStudent={true} />
@@ -10,6 +20,7 @@ function App() {
       <Student name="Barcio Senior" age={48} isStudent={false} />
       <Student />
       <UserGreeting isLoggedIn={true} username="HighImExy" />
+      <List items={fruits} category="Fruits"/>
       <Button></Button>
     </>
   );
---
+++ src/Components/List.tsx
@@ -0,0 +1,31 @@
+export type Fruit = {
+    id: number
+    name: string
+    kcal: number
+}
+
+type ListProps = {
+    items: Fruit[]
+    category: string
+}
+
+function List(props: ListProps) {
+
+    const itemList = props.items;
+  //fruits.sort((a, b) => a.name.localeCompare(b.name)); //ALPHABETICAL
+  //fruits.sort((a, b) => b.name.localeCompare(a.name)); //REVERSE
+  //fruits.sort((a,b) => a.kcal - b.kcal); // NUMERIC ORDER
+  //fruits.sort((a, b) => b.kcal - a.kcal); // REVERSE NUMERIC ORDER
+
+  // const lowCalFruits = fruits.filter(fruit => fruit.kcal < 100);
+  //const highCalFruits = fruits.filter(fruit => fruit.kcal > 100);
+
+  const listItems = itemList.map((item) => (
+    <li key={item.id}>
+      {item.name}: &nbsp; <b>{item.kcal}</b>
+    </li>
+  ));
+  return <ol>{listItems}</ol>;
+}
+
+export default List;
