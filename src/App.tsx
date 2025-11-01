65766c070814f48fa588d7699986960e886f3e0c
highimexy <teslawiktor@gmail.com>
2025-11-01 15:44:23 +0100

    45:57

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Button from "./assets/Button/Button.tsx";
+import Student from "./assets/Components/Student.tsx";
 
 function App() {
   return (
     <>
+      <Student name="Sponge Bob" age={30} isStudent={true}/>
       <Button></Button>
     </>
   );
