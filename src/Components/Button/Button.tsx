6154c6fb9d38238ffa19b511fb5e6c758b4be426
highimexy <teslawiktor@gmail.com>
2025-11-02 20:57:35 +0100

    1h:37

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,7 +1,8 @@
 import styles from "./Button.module.css";
 
 function Button() {
-  let count = 0;
+  const handleClick = (e) => console.log(e);
+  /* let count = 0;
   const handleClick = (name: string) => {
     if (count < 3){
       count++;
@@ -9,13 +10,12 @@ function Button() {
     } else {
       console.log(`${name} stop clicking me `);
     }
-  };
+  }; */
   return (
-  <button onClick={() => handleClick("Bro")} className={styles.brobutton}>
-    Click me 😀
-  </button>
-)}
-
-
+    <button onClick={(e) => handleClick(e)} className={styles.brobutton}>
+      Click me 😀
+    </button>
+  );
+}
 
 export default Button;
