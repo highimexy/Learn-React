335a1f0da38ec5575b87d8f8178bf1c2f9818e4b
highimexy <teslawiktor@gmail.com>
2025-11-03 19:01:54 +0100

    1h:52

--- src/Components/MyComponent.tsx
+++ src/Components/MyComponent.tsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 
 function MyComponent() {
   const [name, setName] = useState("Guest");
-
   const [age, setAge] = useState(0);
+  const [isEmployed, setIsEmployed] = useState(false);
+
   const updateName = () => {
     setName("Highimexy");
   };
 
   const incrementAge = () => {
-    setAge(age + 2)
+    setAge(age + 2);
+  };
+
+  const toggleEmployedStatus = () => {
+    setIsEmployed(!isEmployed);
   };
   return (
     <div>
@@ -18,6 +23,9 @@ function MyComponent() {
 
       <p>Age: {age}</p>
       <button onClick={incrementAge}>Increment Age</button>
+
+      <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
+      <button onClick={toggleEmployedStatus}>Toggle Status</button>
     </div>
   );
 }
