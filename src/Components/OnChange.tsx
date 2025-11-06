ced1c192ec8b8f151a44ea019a53fecb2514c6c5
highimexy <teslawiktor@gmail.com>
2025-11-06 19:45:48 +0100

    2h:13

--- src/Components/OnChange.tsx
+++ src/Components/OnChange.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Divider from "./Divider";
 
 function OnChange() {
   const [name, setName] = useState("");
-
   const [quantity, setQuantity] = useState(1);
-
   const [comment, setComment] = useState("");
+  const [payment, setPayment] = useState("");
+  const [shipping, setShipping] = useState("");
 
   function handleNameChange(event: any) {
     setName(event.target.value);
@@ -18,6 +19,15 @@ function OnChange() {
   function handleCommentChange(event: any) {
     setComment(event.target.value);
   }
+
+  function handlePaymentChange(event: any) {
+    setPayment(event.target.value);
+  }
+
+  function handeShippingChange(event: any) {
+    setShipping(event.target.value);
+  }
+
   return (
     <>
       <div className="onchange-container">
@@ -30,13 +40,43 @@ function OnChange() {
           onChange={handleQuantityChange}
         ></input>
         <p>Quantity: {quantity}</p>
-
+        <Divider />
         <textarea
           placeholder="Leave a message for specific delivery instruction"
           value={comment}
           onChange={handleCommentChange}
         ></textarea>
         <p>Comment: {comment}</p>
+        <Divider />
+        <select value={payment} onChange={handlePaymentChange}>
+          <option value="">Select an option</option>
+          <option value="Visa">Visa</option>
+          <option value="Mastercard">Mastercard</option>
+          <option value="Giftcard">Giftcard</option>
+        </select>
+        <p>Payment: {payment}</p>
+        <Divider />
+        <div className="onchange-radio">
+          <label>
+            <input
+              type="radio"
+              value="Pick Up"
+              checked={shipping === "Pick Up"}
+              onChange={handeShippingChange}
+            />
+            Pick Up
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="Delivery"
+              checked={shipping === "Delivery"}
+              onChange={handeShippingChange}
+            />
+            Delivery
+          </label>
+        </div>
+        <p>Shipping: {shipping}</p>
       </div>
     </>
   );
